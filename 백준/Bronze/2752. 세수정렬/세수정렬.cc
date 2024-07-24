70c3d48b372e309b4f7b2c0743e9ca6a3621bd6e
follro <108092605+follro@users.noreply.github.com>
2024-07-24 18:45:51 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int a[3], temp;
+
+	cin >> a[0] >> a[1] >> a[2];
+
+	
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = i + 1; j < 3; j++)
+		{
+			if (a[i] > a[j])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	
+
+	for (int i = 0; i < 3; i++)
+		cout << a[i] <<" ";
+
+
+
+	
+
+}
