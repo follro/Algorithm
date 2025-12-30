059b0b96d1f2ef0c7d0bca9868e382186a95ad83
Lee Hak Seung <108092605+follro@users.noreply.github.com>
2025-12-30 20:49:58 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 12 ms, Memory: 6060 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -2,56 +2,60 @@
 #include<queue>
 #define MAX 500001
 
+int n, k, turn = 1;
 int visited[2][MAX];
-
-int n, k, meetT;
-std::queue<std::pair<int,int>> q;
-
-void bfs()
-{
-	std::fill(&visited[0][0], &visited[0][0] + MAX * 2, -1);
-	visited[0][n] = 0;
-	q.push({ n, 0 });// 위치와 시간 표시
-
-	while (!q.empty())
-	{
-		int curPos = q.front().first;
-		int curTime = q.front().second;
-		q.pop();
-
-		for (int nextPos : {curPos - 1, curPos + 1, curPos * 2})
-		{
-			int nextTime = curTime + 1;
-			if (nextPos < 0 || nextPos >= MAX || visited[nextTime % 2][nextPos] != -1) continue;
-
-			q.push({nextPos, nextTime});
-			visited[nextTime % 2][nextPos] = visited[curTime % 2][curPos] + 1;
-		}
-	}
-
-	return;
-}
+std::queue<int> q;
 
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL); std::cout.tie(NULL);
-
 	std::cin >> n >> k;
-	bfs();
-	for (int i = 0; i < MAX; i++) 
+	if (n == k)
 	{
-		k += i; 
+		std::cout << "0";
+		return 0;
+	}
+	q.push(n);
+	visited[0][n];
 
-		if (k >= MAX) break; 
-
-		if (visited[i % 2][k] != -1 && visited[i % 2][k] <= i)
+	bool trigger = false;
+	while (!q.empty())
+	{
+		k += turn;
+		if (k >= MAX) break;
+		if (visited[turn % 2][k])
 		{
-			std::cout << i;
-			return 0;
+			trigger = true;
+			break;
 		}
+
+		int qSize = q.size();
+		for (int i = 0; i < qSize; i++)
+		{
+			int cur = q.front();
+			q.pop();
+			for (int next : {cur - 1, cur + 1, cur * 2})
+			{
+				if (next < 0 || next >= MAX || visited[turn % 2][next]) continue;
+				visited[turn % 2][next] = visited[(turn + 1) % 2][cur] + 1;
+				if (next == k)
+				{
+					trigger = true;
+					break;
+				}
+				q.push(next);
+			}
+			if (trigger) break;
+		}
+
+		if (trigger) break;
+		turn++;
 	}
 
-	std::cout << "-1";
+	if (trigger)
+		std::cout << turn;
+	else
+		std::cout << "-1";
 	return 0;
 }
